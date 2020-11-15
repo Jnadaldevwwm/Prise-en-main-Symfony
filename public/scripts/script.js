e8ef55c7ee11f6e66afa4475221a6e075c837847
Jnadaldevwwm <68844488+Jnadaldevwwm@users.noreply.github.com>
2020-11-15 13:49:03 +0100

    Form

---
+++ public/scripts/script.js
@@ -0,0 +1,4 @@
+var boutonAjout = document.getElementById('bAjout');
+boutonAjout.addEventListener('click', function(){
+    console.log('alo');
+})
--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -5,6 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Photo;
 
 class PhotoController extends AbstractController
@@ -21,5 +27,38 @@ class PhotoController extends AbstractController
         return $this->render('photo/index.html.twig', [
             'controller_name' => 'PhotoController', 'photos' => $photos
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $requete, EntityManagerInterface $em): Response{
+        $form = $this->createFormBuilder()
+                ->add('titre', TextType::class)
+                ->add('annee', IntegerType::class)
+                ->add('prix', NumberType::class)
+                ->add('visuel', TextType::class)
+                ->add('submit', SubmitType::class, ['label' => 'Ajout Photo'])
+                ->getForm();
+
+        $form->handleRequest($requete);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $photo = new Photo;
+            $photo->setTitre($data['titre']);
+            $photo->setAnnee($data['annee']);
+            $photo->setPrix($data['prix']);
+            $photo->setVisuel($data['visuel']);
+            $em->persist($photo);
+            $em->flush();
+
+            return $this->redirectToRoute('photo');
+        }
+
+        return $this->render('photo/create.html.twig', [
+                    'controller_name' => 'PhotoController', 'formulaire' => $form->createView()
+                ]);
+       
     }
 }
